ca0ae531f38c63d58a38802085c4be190a7af64d
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-20 16:05:42 +0200

    Add joint tables and created foreign key indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -37,3 +37,19 @@ CREATE TABLE invoice_items (
     treatment_id INT REFERENCES treatments(id),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE medical_histories_treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    medical_history_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+    CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments (id),
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX patients_index ON medical_histories(patient_id);
+CREATE INDEX invoice_items_index ON invoice_items(invoice_id);
+CREATE INDEX treatment_index ON invoice_items(treatment_id); 
+CREATE INDEX invoices_index ON invoices(medical_history_id);
+CREATE INDEX medical_history_id_index ON  medical_histories_treatments(medical_history_id);
+CREATE INDEX treatment_id_index ON  medical_histories_treatments(treatment_id);

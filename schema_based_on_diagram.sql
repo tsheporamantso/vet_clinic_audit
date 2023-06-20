e52c50873edb5309212201d3f247b9922bf93ba0
Benson Njuguna <njugunab655@gmail.com>
2023-06-20 16:17:21 +0300

    Add invoices and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,7 +7,7 @@ CREATE TABLE patients (
 
 CREATE TABLE medical_histories(
     id INT GENERATED ALWAYS AS IDENTITY,
-    admitted_at timestamp,
+    admitted_at TIMESTAMP,
     patient_id INT REFERENCES patients(id),
     status VARCHAR,
     PRIMARY KEY(id) 
@@ -17,5 +17,23 @@ CREATE TABLE treatments(
     id INT GENERATED ALWAYS AS IDENTITY,
     type VARCHAR,
     name VARCHAR,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id),
     PRIMARY KEY(id)
 );
